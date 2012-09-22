fed8fefffb9de5fdee40c0768d596f589e6ec3d7
Kares <kares.qin@gmail.com>
2012-09-22 14:47:06 +0800

    time,js

--- script/timer.js
+++ script/timer.js
@@ -1,5 +1,13 @@
 //time.js
 
 define(function(require,exports,module){
+var timeoutFns = [];
+
+	exports.on = function(type, fn){
+		switch (type){
+			case "timeout":
+				timeoutFns.push(fn);
+		}
+	}
     
 });
