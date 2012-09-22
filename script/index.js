eda4c0b248619b797045aea092a62a527db641c4
Kares <kares.qin@gmail.com>
2012-09-22 16:31:22 +0800

    a big fix

--- script/index.js
+++ script/index.js
@@ -6,8 +6,12 @@ seajs.config({
 });
 
 define(function(require,exports,module){
-
+	var time = require('./timer')
 	var module = require('./module');
+
+	time.on("timeout", function(num){
+
+	})
 
 	module.init();
     
